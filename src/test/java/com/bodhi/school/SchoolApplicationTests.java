2b7fabc22b9d6698c454471e11935729802245c6
Deepak Gami <deepak.gami@mavq.com>
2024-11-10 16:55:36 +0530

    initiated spring project for school management system

---
+++ src/test/java/com/bodhi/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodhi.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
